18b654823eba2e792b88f8ee03f87c947cad484f
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-17 01:26:46 -0700

    add /total-confirmed endpoint

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.8.1"]
                  [compojure "1.6.1"]
+                 [incanter "1.9.3"]
+                 [tupelo "0.9.201"]
                  [metosin/muuntaja "0.6.7"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler covid-ring-compojure.web/app}
--- src/covid_ring_compojure/web.clj
+++ src/covid_ring_compojure/web.clj
@@ -8,11 +8,20 @@
         [ring.middleware.file-info :only (wrap-file-info)]
         [ring.middleware.stacktrace :only (wrap-stacktrace)]
         [ring.util.response :only (redirect)]
+        [incanter.core :as i]
+        [incanter.io :refer [read-dataset]]
         [muuntaja.middleware :as mw]))
 
+(def covid-data (read-dataset "06-08-2020.csv" :header true))
+
+(defn total-confirmed []
+  (reduce + (i/$ :Confirmed covid-data)))
+
+(def total-confirmed-memo (memoize total-confirmed))
 
 (defroutes site-routes
   (GET "/" [] (redirect "/data/census-race.json"))
+  (GET "/total-confirmed" [] (str (total-confirmed-memo)))
   (route/resources "/")
   (route/not-found "Page not found"))
 
