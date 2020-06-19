c6bb59867c020cf6d2a5fb8e5e5d547c464d0706
Grant Surlyn <grant@sunflowerseastar.com>
2020-06-18 22:52:04 -0700

    add us-state-level-deaths-recovered; cleanup

--- src/covid_server/core.clj
+++ src/covid_server/core.clj
@@ -12,41 +12,43 @@
         [incanter.io :refer [read-dataset]]
         [muuntaja.middleware :as mw]))
 
-(def csse-daily-reports (read-dataset "06-08-2020.csv" :header true))
+(def csse-daily-report (read-dataset "06-08-2020.csv" :header true))
+(def csse-daily-report-us (read-dataset "states_06-18-2020.csv" :header true))
 
-(defn total-confirmed []
-  (reduce + (i/$ :Confirmed csse-daily-reports)))
-
-(defn global-deaths []
-  (let [total-deaths (reduce + (i/$ :Deaths csse-daily-reports))
-        deaths-by-region (->> (i/$rollup :sum :Deaths :Country_Region csse-daily-reports)
-                              (i/$order :Deaths :desc)
-                              to-vect)]
-    {:deaths-by-region deaths-by-region
-     :total-deaths total-deaths}))
-
-(def total-confirmed-memo (memoize total-confirmed))
-
-(defn confirmed-by-region []
-  (->> (i/$rollup :sum :Confirmed :Country_Region csse-daily-reports)
+(defn confirmed-by-region [data]
+  (->> data (i/$rollup :sum :Confirmed :Country_Region)
        (i/$order :Confirmed :desc)
        to-vect))
 
+(defn global-deaths [data]
+  {:deaths-by-region (->> data (i/$rollup :sum :Deaths :Country_Region)
+                          (i/$order :Deaths :desc)
+                          to-vect)
+   :total-deaths (reduce + (i/$ :Deaths data))})
+
+(defn total-confirmed [data]
+  (reduce + (i/$ :Confirmed data)))
+
+(defn us-state-level-deaths-recovered [data-us]
+  (->> data-us
+       (i/$ [:Province_State :Deaths :Recovered])
+       (i/$order :Deaths :desc)
+       (to-vect)))
+
 (defroutes site-routes
   (GET "/" [] "")
-  (GET "/global-deaths" [] (str (global-deaths)))
-  (GET "/us-state-level-deaths-recovered" [] (str (us-state-level-deaths-recovered)))
-  (GET "/total-confirmed" [] (str (total-confirmed-memo)))
-  (GET "/confirmed-by-region" [] (str (confirmed-by-region)))
-  (GET "/all" [] {:body {:total-confirmed (total-confirmed-memo)
-                         :global-deaths (global-deaths)
-                         :confirmed-by-region (confirmed-by-region)}})
-  (route/resources "/")
+  (GET "/confirmed-by-region" [] (str (confirmed-by-region csse-daily-report)))
+  (GET "/global-deaths" [] (str (global-deaths csse-daily-report)))
+  (GET "/total-confirmed" [] (str (total-confirmed csse-daily-report)))
+  (GET "/us-state-level-deaths-recovered" [] (str (us-state-level-deaths-recovered csse-daily-report-us)))
+  (GET "/all" [] {:body {:confirmed-by-region (confirmed-by-region csse-daily-report)
+                         :global-deaths (global-deaths csse-daily-report)
+                         :total-confirmed (total-confirmed csse-daily-report)
+                         :us-state-level-deaths-recovered (us-state-level-deaths-recovered csse-daily-report-us)}})
   (route/not-found "Page not found"))
 
 (def api
   (-> (handler/site site-routes)
-      (wrap-file "resources")
       (wrap-file-info)
       (mw/wrap-format)
       (wrap-content-type)))
