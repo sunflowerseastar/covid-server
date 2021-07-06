f02fc0222adc3c70b5799e52a29d5d815176514c
Grant Surlyn <grant@sunflowerseastar.com>
2021-07-06 09:39:34 -0400

    readme, license, bump ver

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject covid-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject covid-server "0.2.0"
+  :description "Data pipeline + API for covid-dashboard"
+  :url "https://covid-dashboard.sunflowerseastar.com"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [ring "1.8.1"]
+                 [ring-cors "0.1.13"]
                  [compojure "1.6.1"]
                  [incanter "1.9.3"]
                  [tupelo "0.9.201"]
